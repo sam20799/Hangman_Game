88341e60e3ee50c416e71ef19dfc74e3c63e0f88
sam <sam@gmail.com>
2024-11-29 19:56:59 +0530

    single letter guess

--- function.py
+++ function.py
@@ -1,10 +1,45 @@
-print("Hello")
-total_char = len("Hello")
-print(total_char)
+import random
 
-def function():
-    print("SAM")
-    print(len("sam"))
+# print("Hello")
+# total_char = len("Hello")
+# print(total_char)
+#
+# def function():
+#     print("SAM")
+#     print(len("sam"))
+#
+#
+# for i in range(6):
+#     print(i)
 
 
-function()
+names = ["sam","dam","obam","sahzam"]
+
+random_name = (random.choice(names))
+print(random_name)
+
+size = len(random_name)
+
+placeholder = ""
+for i in range(size):
+    placeholder += "_"
+
+print(placeholder)
+
+user_input = input("Guess a letter: ").lower()
+# print(user_input)
+
+
+display = ""
+
+
+for letter in random_name :
+    if letter == user_input:
+        display += letter
+
+    else:
+        display+= "_"
+
+print(display)
+
+
